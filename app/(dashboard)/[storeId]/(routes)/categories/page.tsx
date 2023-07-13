53b6efd96abc6b8b7217d4e843bcb79f6d5c39f7
zak <mzhussain092@gmail.com>
2023-07-13 16:24:30 +0530

    add Categories page and it's APIs

---
+++ app/(dashboard)/[storeId]/(routes)/categories/page.tsx
@@ -0,0 +1,34 @@
+﻿import prismadb from "@/lib/prismadb";
+import { format } from "date-fns";
+import { CategoryColumn } from "./components/Columns";
+import CategoriesClient from "./components/CategoryClient";
+
+const CategoryPage = async ({ params }: { params: { storeId: string } }) => {
+  const categories = await prismadb.category.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    include: {
+      billboard: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const formattedCategories: CategoryColumn[] = categories.map((category) => ({
+    id: category.id,
+    name: category.name,
+    billboardLabel: category.billboard.label,
+    createdAt: format(category.createdAt, "MMMM do, yyyy"),
+  }));
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <CategoriesClient data={formattedCategories} />
+      </div>
+    </div>
+  );
+};
+
+export default CategoryPage;
--- components/Main-Nav.tsx
+++ components/Main-Nav.tsx
@@ -28,6 +28,11 @@ const MainNav: FC<MainNavProps> = ({
       active: pathname === `/${params.storeId}/billboards`,
     },
     {
+      href: `/${params.storeId}/categories`,
+      label: "Categories",
+      active: pathname === `/${params.storeId}/categories`,
+    },
+    {
       href: `/${params.storeId}/settings`,
       label: "Settings",
       active: pathname === `/${params.storeId}/settings`,
