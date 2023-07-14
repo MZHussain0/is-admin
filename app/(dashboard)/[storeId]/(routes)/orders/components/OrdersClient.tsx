fc2fd2d7fb0cc776765d9e4fb043935fc9f2bba9
zak <mzhussain092@gmail.com>
2023-07-14 19:15:19 +0530

    Add Orders Page and Orders API

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/OrdersClient.tsx
@@ -0,0 +1,30 @@
+﻿"use client";
+import { FC } from "react";
+
+import Heading from "@/components/ui/Heading";
+import { Separator } from "@/components/ui/separator";
+import { DataTable } from "@/components/ui/data-table";
+
+import { OrderColumns, columns } from "./Columns";
+
+interface OrdersClientProps {
+  data: OrderColumns[];
+}
+
+const OrdersClient: FC<OrdersClientProps> = ({ data }) => {
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Orders (${data?.length}) `}
+          description="Manage orders for your store"
+        />
+      </div>
+      <Separator />
+
+      <DataTable columns={columns} data={data} searchKey="products" />
+    </>
+  );
+};
+
+export default OrdersClient;
